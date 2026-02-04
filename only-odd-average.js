993cbcf30c4f66b71e67b2ceb0da9ed1e89bc3fd
Zihad-1883 <zihad.pilot18@gmail.com>
2026-02-05 03:33:03 +0600

    correction on odd average

--- only-odd-average.js
+++ only-odd-average.js
@@ -1,14 +1,17 @@
 function oddAverage(){
     let sum = 0;
+    let odd = [];
     for(const num of number){
         if(num % 2 !== 0){
             sum += num;
+            odd.push(num);
         }
         else{
             sum += 0;
         }
     }
-    return sum;
+    let avg = sum / odd.length;
+    return avg;
 }
 
 let number = [1,2,3,4,5,6,7];
